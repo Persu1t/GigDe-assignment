7fde25f73fb3ce651f00ed5d688b6cf69db89a90
Rishabh <shuklarishabh890@gamil.com>
2025-05-03 14:08:31 +0530

    Updated the frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://task-tracker-eta-eight.vercel.app/",
         credentials: true,
     })
 );
